1341ac08853dbdeb867251d554755436b423a482
Oscar Louw <ojlou1@student.monash.edu>
2015-04-08 16:01:12 +1000

    Project setup
    Set up project with relevant scripts

---
+++ PlatformerControls/Assets/CameraMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+    public Transform target;
+
+    public float camSpeed = 2f;
+
+	void Update () {
+
+        transform.position = Vector3.Slerp(transform.position, new Vector3(target.position.x, target.position.y, transform.position.z), Time.deltaTime * camSpeed);
+
+	}
+}
---
+++ PlatformerControls/Assets/PlayerMovement.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+enum Direction
+{
+    left,
+    right
+}
+
+public class PlayerMovement : MonoBehaviour {
+
+    
+
+
+
+    public KeyCode[] left, right, jump;
+
+    public float jumpHeight;
+    public float walkSpeed;
+
+
+    private Rigidbody rigidbody;
+
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	
+	void Update () {
+        // Handle all the possible input codes for player movement
+
+        // Pressing left key
+        for(int i = 0; i < left.Length; i++)
+        {
+            if (Input.GetKey(left[i]))
+                MoveDirection(Direction.left);
+        }
+
+        // Pressing right key
+        for (int i = 0; i < right.Length; i++)
+        {
+            if (Input.GetKey(right[i]))
+                MoveDirection(Direction.right);
+        }
+
+        // Pressing jump
+        for (int i = 0; i < jump.Length; i++)
+        {
+            if (Input.GetKeyDown(jump[i]))
+                Jump();
+        }
+	}
+
+    void MoveDirection(Direction direction)
+    {
+
+    }
+
+    void Jump()
+    {
+        rigidbody.AddForce(Vector3.up * jumpHeight);
+    }
+
+
+}
